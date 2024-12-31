33bbe1d723f69dc5e02219fccc2193bb44e9928a
rohit negi <rohitnegi263139@gmail.com>
2024-12-31 21:18:58 +0530

    add Login functionality

--- frontend/src/Pages/login/Login.tsx
+++ frontend/src/Pages/login/Login.tsx
@@ -1,7 +1,16 @@
-import React from "react";
 import { Link } from "react-router";
+import useLogin from "../../hooks/useLogin";
+import { FormEvent, FormEventHandler, useState } from "react";
 
 function Login() {
+	const { loading, login } = useLogin();
+	const [formData, setFormData] = useState({ username: "", password: "" });
+	const handleSubmit: FormEventHandler<HTMLFormElement> = (
+		e: FormEvent
+	): void => {
+		e.preventDefault();
+		login(formData);
+	};
 	return (
 		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
 			<div className="w-full p-6 bg-gray-400 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 shadow-md ">
@@ -9,12 +18,15 @@ function Login() {
 					Login
 					<span className="text-blue-500">ChatSphere</span>
 				</h1>
-				<form className="text-white">
+				<form onSubmit={handleSubmit} className="text-white">
 					<div className="text-white">
 						<label className="label p-2">
 							<span className="text-base label-text text-white">Username</span>
 						</label>
 						<input
+							onChange={(e) =>
+								setFormData({ ...formData, username: e.target.value })
+							}
 							type="text"
 							placeholder="Enter username"
 							className="w-full input input-bordered input-primary h-10 bg-black "
@@ -25,6 +37,9 @@ function Login() {
 							<span className="text-base label-text text-white">Password</span>
 						</label>
 						<input
+							onChange={(e) =>
+								setFormData({ ...formData, password: e.target.value })
+							}
 							type="text"
 							placeholder="Enter password"
 							className="w-full input input-bordered input-primary h-10 bg-black "
@@ -38,7 +53,11 @@ function Login() {
 					</Link>
 
 					<button className="btn btn-block btn-sm mt-2 btn-primary">
-						Login
+						{loading ? (
+							<span className="loading loading-spinner text-secondary"></span>
+						) : (
+							"Login"
+						)}
 					</button>
 				</form>
 			</div>
---
+++ frontend/src/hooks/useLogin.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useAuthContext } from "../context/AuthContext";
+
+const useLogin = () => {
+	const [loading, setLoading] = useState(false);
+	const { setAuthUser } = useAuthContext();
+	const login = async (formData: { username: string; password: string }) => {
+		try {
+			setLoading(true);
+			const res = await fetch("http://localhost:7000/api/auth/signin", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.error) {
+				throw new Error(data.error);
+			}
+			setAuthUser(data);
+			toast.success(`Wellcome back ${data.username}`);
+		} catch (error: Error | any) {
+			toast.error(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
+	return { loading, login };
+};
+export default useLogin;
