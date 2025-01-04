9160f4cb56ad481c7518924ecb701dd471ec32dc
rohit negi <rohitnegi263139@gmail.com>
2025-01-04 14:49:23 +0530

    deployment setup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv";
 import express, { urlencoded } from "express";
 import authRoute from "./routes/auth.route.js";
@@ -6,9 +7,11 @@ import userRoute from "./routes/user.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 7001;
+
+const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +19,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
 	connectMongoDB();
 	console.log(`Server started! at port: ${PORT}`);
